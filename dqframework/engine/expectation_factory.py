1b251479ee7487854c2a37a192feee84a2acec81
lavanya <lavanyakanaka1234@gmail.com>
2026-07-30 13:31:18 +0530

    Fix expectation factory and validation mappings

--- dqframework/engine/expectation_factory.py
+++ dqframework/engine/expectation_factory.py
@@ -1,13 +1,32 @@
-@classmethod
-def create(cls, config):
+from dqframework.expectations.not_null import NotNullExpectation
+from dqframework.expectations.unique import UniqueExpectation
+from dqframework.expectations.between import BetweenExpectation
+from dqframework.expectations.in_set import InSetExpectation
+from dqframework.expectations.mean_between import MeanBetweenExpectation
+from dqframework.expectations.row_count import RowCountExpectation
+from dqframework.expectations.type_check import TypeExpectation
 
-    expectation_type = config.get("type")
 
-    if expectation_type not in cls.EXPECTATIONS:
-        raise ValueError(
-            f"Unsupported expectation: {expectation_type}"
-        )
+class ExpectationFactory:
 
-    return cls.EXPECTATIONS[
-        expectation_type
-    ](config)
+    EXPECTATIONS = {
+        "expect_column_to_not_be_null": NotNullExpectation,
+        "expect_column_values_to_be_unique": UniqueExpectation,
+        "expect_column_values_to_be_between": BetweenExpectation,
+        "expect_column_values_to_be_in_set": InSetExpectation,
+        "expect_column_mean_to_be_between": MeanBetweenExpectation,
+        "expect_table_row_count_to_be_between": RowCountExpectation,
+        "expect_column_values_to_be_of_type": TypeExpectation
+    }
+
+    @classmethod
+    def create(cls, config):
+
+        expectation_type = config.get("type")
+
+        if expectation_type not in cls.EXPECTATIONS:
+            raise ValueError(
+                f"Unsupported expectation: {expectation_type}"
+            )
+
+        return cls.EXPECTATIONS[expectation_type](config)
